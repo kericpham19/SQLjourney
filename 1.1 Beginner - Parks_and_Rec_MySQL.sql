9edaa850f3a77ee7a0ab4bef238a686893eace3c
kericpham19 <131621286+kericpham19@users.noreply.github.com>
2024-08-02 20:31:10 -0400

    Add files via upload

---
+++ 1.1 Beginner - Parks_and_Rec_MySQL.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM parks_and_recreation.employee_salary;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics;
+
+## Lighting bolt with "I" runs the code that is selected wheras the lightingbolt without runs every code in this queary. 
